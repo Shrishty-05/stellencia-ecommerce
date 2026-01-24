488d5fbd1dd680625596b4f6e92c54e9214212d6
Shrishty-05 <shrishtyalanse05@gmail.com>
2026-01-24 16:11:41 +0530

    Add common components folder

---
+++ src/components/common/Divider.jsx
@@ -0,0 +1,7 @@
+export default function Divider(){
+    return <>
+    <div className="w-full h-1 mt-1 bg-linear-to-r from-transparent via-[maroon] to-transparent" />
+
+                {/* <div className="mx-2 mt-1 h-0.5 bg-[maroon] opacity-100" /> */}
+    </>
+}
